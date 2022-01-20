2433c1f3e031c80ea0ea7600a65ccbd4a4bc9c5e
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-20 18:31:24 +0100

    Rendering random numbers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Die from "./Die"
 
 export default function App() {
+
+    const [dice, setDice] = useState(allNewDice())
+
+    function allNewDice() {
+        const newDice = []
+        for (let index = 0; index < 10; index++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="5" />
-                <Die value="4" />
-                <Die value="6" />
-                <Die value="2" />
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="3" />
-                <Die value="1" />
+                {diceElements}
             </div>
         </main>
     )

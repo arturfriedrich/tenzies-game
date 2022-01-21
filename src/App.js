06911696bfc1c8ba3cb84d68ee912d4cbc61868b
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-21 12:21:19 +0100

    Added restart

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ export default function App() {
         const allSameValue = dice.every(die => die.value === firstValue)
         if (allHeld & allSameValue) {
             setTenzies(true)
-            console.log("You won")
         }
     }, [dice])
 
@@ -37,11 +36,16 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice(oldDice => oldDice.map(die => {
-            return die.isHeld ?
-                die :
-                generateNewDie()
-        }))
+        if (!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ?
+                    die :
+                    generateNewDie()
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+        }
     }
 
     function holdDice(id) {
